109518373e7cfa36a712c3bc1f14ad28ddec526f
Max Tamer-Mahoney <max@mxtm.me>
2013-01-12 21:52:37 -0500

    Made cron scripts only able to be used from CLI

--- vulcan/adminpanel/enablesignupscron.php
+++ vulcan/adminpanel/enablesignupscron.php
@@ -1,7 +1,12 @@
 <?php
-require("../settings.inc.php");
-mysql_connect(mysqlip, mysqluser, mysqlpw) or die(mysql_error());
-mysql_select_db(mysqldb) or die(mysql_error());
-$query = "UPDATE signupSettings SET disabled=0 WHERE disabled=1";
-mysql_query($query) or die(mysql_error());
+if (php_sapi_name() == "cli") {
+	require("../settings.inc.php");
+	mysql_connect(mysqlip, mysqluser, mysqlpw) or die(mysql_error());
+	mysql_select_db(mysqldb) or die(mysql_error());
+	$query = "UPDATE signupSettings SET disabled=0 WHERE disabled=1";
+	mysql_query($query) or die(mysql_error());
+}
+else {
+	echo "Sorry, this script is only able to be executed from the console.";
+}
 ?>
