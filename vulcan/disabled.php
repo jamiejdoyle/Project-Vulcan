c3a745c8afc8cfbc8b82db32b7ba0fdea59e48ae
jamiejdoyle <jamiej.doyle@gmail.com>
2013-03-28 13:31:13 -0300

    Update disabled.php
    i added a link to the github

--- vulcan/disabled.php
+++ vulcan/disabled.php
@@ -1,3 +1,4 @@
-The library signups are currently closed. They are open from 6:00 PM to 7:25 AM on weekdays, and 24/7 on the weekends.
+The library signups are currently closed. They are open from 7:00 PM to 7:25 AM on weekdays, and 24/7 on the weekends.
 <br><br>
 If you are seeing this message and the signups should be opened, email me at <a href="mailto:max@mxtm.me">max@mxtm.me</a>.
+to view the open source version click on <a href="https://github.com/mxtm/Project-Vulcan/edit/master/vulcan"> this link </a>
